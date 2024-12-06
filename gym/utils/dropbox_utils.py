adc22708683288b424e1ac1a7e76c55ad39fdf84
jeevjacobgeorge <jeev@J4Hp>
2024-12-06 15:14:31 +0530

    added dotenv

--- gym/utils/dropbox_utils.py
+++ gym/utils/dropbox_utils.py
@@ -1,11 +1,12 @@
 import dropbox
 import os
 from django.conf import settings
-
 def upload_to_dropbox(file_path, dropbox_path):
-    access_token = settings.DROPBOX_ACCESS_TOKEN
+    access_token = os.getenv("DROPBOX_ACCESS_TOKEN")
     if not access_token:
-        raise ValueError("Dropbox access token is not set.")
+        access_token = settings.DROPBOX_ACCESS_TOKEN
+        if not access_token:
+            raise ValueError("Dropbox access token is not set.")
     dbx = dropbox.Dropbox(access_token)
     with open(file_path, "rb") as f:
-        dbx.files_upload(f.read(), dropbox_path, mode=dropbox.files.WriteMode.overwrite)
+        dbx.files_upload(f.read(), dropbox_path)
