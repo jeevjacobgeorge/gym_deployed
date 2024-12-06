a82ccb703d2bf80b4aa67cd77e50b5e34e83aed8
jeevjacobgeorge <jeev@J4Hp>
2024-12-06 13:06:01 +0530

    path changed

--- gym/utils/dropbox_utils.py
+++ gym/utils/dropbox_utils.py
@@ -4,6 +4,7 @@ from django.conf import settings
 
 def upload_to_dropbox(local_path, dropbox_path):
     access_token = settings.DROPBOX_ACCESS_TOKEN  # Store your token in Django settings
+    print(f"Access Token: {access_token}")
     dbx = dropbox.Dropbox(access_token)
     with open(local_path, "rb") as f:
         dbx.files_upload(f.read(), dropbox_path, mode=dropbox.files.WriteMode("overwrite"))
