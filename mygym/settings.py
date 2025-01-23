5d9db42681f223ffb2a134651d3136c4d64b4f40
jeevjacobgeorge <jeev@J4Hp>
2025-01-23 20:53:15 +0530

    Changed password

--- mygym/settings.py
+++ mygym/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         },
         'NAME': 'gym',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'gads@123',
         'HOST': '127.0.0.1',
         'PORT': '3306',
 
